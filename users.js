13ab4b8db9137a6d294fc14998f68e48b1182d38
John Crisostomo <johncrisostomo@protonmail.ch>
2017-03-20 13:39:42 +0800

    adds fake user store

---
+++ users.js
@@ -0,0 +1,14 @@
+const users = [
+  { _id: 1, name: "john", email: "john@crisostomo.com", password: "john12345" },
+  { _id: 2, name: "crisostomo", email: "crisostomo@john.com", password: "crisostomo12345" },
+];
+
+function validateUser(username, password) {
+  const user = users.find((user) => {
+    return user.name === username && user.password === password;
+  });
+
+  return user;
+}
+
+module.exports = validateUser;
